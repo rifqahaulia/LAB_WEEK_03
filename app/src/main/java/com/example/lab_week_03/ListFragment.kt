37bc9f36e53aeec7bf16a5e2fa11ca5287b8539d
rifqah aulia <rifqahauliasalsabilla173@gmail.com>
2025-09-16 16:41:28 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -5,32 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ListFragment : Fragment(), View.OnClickListener{
-    private var param1: String? = null
-    private var param2: String? = null
-    private lateinit var coffeeListener: CoffeeListener;
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if(context is CoffeeListener){
-            coffeeListener = context
-        }
-        else{
-            throw RuntimeException("Must implement CoffeeListener")
-        }
+class ListFragment : Fragment(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
-    override fun onCreate(...)
-    override fun onCreateView(...)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val coffeeList = listOf<View>(
@@ -38,16 +25,16 @@ class ListFragment : Fragment(), View.OnClickListener{
             view.findViewById(R.id.americano),
             view.findViewById(R.id.latte)
         )
-        coffeeList.forEach{
-            it.setOnClickListener(this)
-        }
-    }
-    override fun onClick(v: View?) {
-        v?.let{
-                coffee -> coffeeListener.onSelected(coffee.id)
+        coffeeList.forEach{ coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(COFFEE_ID, coffee.id)
+            coffee.setOnClickListener(
+                coffee.findNavController().navigate(
+                    R.id.coffee_id_action, fragmentBundle)
+            )
         }
     }
     companion object {
-        …
+        const val COFFEE_ID = "COFFEE_ID"
     }
 }
--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -1,46 +1,20 @@
 package com.example.lab_week_03
-
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
-interface CoffeeListener {
-    fun onSelected(id: Int)
-}
-class MainActivity : AppCompatActivity(), CoffeeListener{
+import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.ListFragment
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_
-                container)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.
+        fragment_container)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.
+            systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right,
                 systemBars.bottom)
             insets
-        }
-        if(savedInstanceState == null){
-            findViewById<FragmentContainerView>(R.id.fragment_container).let{
-                    containerLayout ->
-                val listFragment = ListFragment()
-                supportFragmentManager.beginTransaction()
-                    .add(containerLayout.id, listFragment)
-                    .commit()
-            }
-        }
-    }
-    override fun onSelected(id: Int){
-// val detailFragment = supportFragmentManager
-// .findFragmentById(R.id.fragment_detail)
-// as DetailFragment
-// detailFragment.setCoffeeData(id)
-        findViewById<FragmentContainerView>(R.id.fragment_container).let{
-                containerLayout ->
-            val detailFragment = DetailFragment.newInstance(id)
-            supportFragmentManager.beginTransaction()
-                .replace(containerLayout.id, detailFragment)
-                .addToBackStack(null)
-                .commit()
         }
     }
 }
